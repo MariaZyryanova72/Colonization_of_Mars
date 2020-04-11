3be714d4330d2993361b88e48c0368092cd17210
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-11 14:04:09 +0500

    Добавим первую запись в таблицу работ.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 from data import db_session
+from data.jobs import Jobs
 from data.users import User
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -13,17 +15,32 @@ def main():
 
 def add_user_func(name, surname=None, age=None, position=None, speciality=None,
                   address=None, email=None, hashed_password=None):
-    user = User()
-    user.surname = surname
-    user.name = name
-    user.age = age
-    user.position = position
-    user.speciality = speciality
-    user.address = address
-    user.email = email
-    user.hashed_password = hashed_password
+    new_user = User()
+    new_user.surname = surname
+    new_user.name = name
+    new_user.age = age
+    new_user.position = position
+    new_user.speciality = speciality
+    new_user.address = address
+    new_user.email = email
+    new_user.hashed_password = hashed_password
     session = db_session.create_session()
-    session.add(user)
+    session.add(new_user)
+    session.commit()
+
+
+def add_job_func(team_leader, job, work_size=None, collaborators=None, start_date=None,
+                 end_date=None, is_finished=None):
+    session = db_session.create_session()
+    new_job = Jobs()
+    new_job.team_leader = team_leader
+    new_job.job = job
+    new_job.work_size = work_size
+    new_job.collaborators = collaborators
+    new_job.start_date = start_date
+    new_job.end_date = end_date
+    new_job.is_finished = is_finished
+    session.add(new_job)
     session.commit()
 
 
@@ -38,6 +55,13 @@ def add_user():
     add_user_func(surname="Yulia", name="Zyryanova", age=18, position="medic",
                   speciality="врач", address="module_3", email="yuila@mars.org")
     return "4 пользователя добавлены в базу данных"
+
+
+@app.route("/add_job")
+def add_job():
+    add_job_func(team_leader=1, job="deployment of residential modules 1 and 2", work_size=15,
+                 collaborators="2, 3", start_date=datetime.datetime.now(), is_finished=False)
+    return "Добавлены в базу данных данные о работе"
 
 
 if __name__ == '__main__':
