82d89b9df872c55c7a5b4d1ed3d12b92a310e59d
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-14 19:17:09 +0500

    Создана новая модель базы данных - Department

---
+++ data/departments.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Department(SqlAlchemyBase):
+    __tablename__ = 'department'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    chief = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    members = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    user_chief = orm.relation("User")
