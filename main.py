fc3ffe7554a96e8c3bb856b40cc6f3378115df0c
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-14 20:00:02 +0500

    + Создаем шаблон формы авторизации на основе базового шаблона миссии. + Внесим изменения в базовый шаблон, чтобы имя залогиненного пользователя отображалось на странице. + Добавляем в файл main.py обработчик адреса /login, который проверяет совпадение логина и пароля пользователя, и в случае совпадения перенаправляет его на главную страницу с отображением списка работ.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
 import datetime
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+from loginform import LoginForm
 from registerform import RegisterForm
 
 app = Flask(__name__)
@@ -109,6 +111,28 @@ def reqister():
 def load_user(user_id):
     session = db_session.create_session()
     return session.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
