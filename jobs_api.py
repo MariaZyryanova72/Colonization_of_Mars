c71b1fa8c0b229a2bd49b01b4469a34b672804dc
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-16 12:55:27 +0500

    Написан обработчик /api/jobs для добавления работы с помощью POST-запроса.

--- jobs_api.py
+++ jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 from data import db_session
 from data.jobs import Jobs
 
@@ -35,3 +35,26 @@ def get_one_jobs(jobs_id):
         }
     )
 
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size',
+                  'collaborators', 'start_date', 'end_date',
+                  'is_finished', 'user.name']):
+        return jsonify({'error': 'Bad request'})
+    session = db_session.create_session()
+    jobs = Jobs(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        start_date=request.json['start_date'],
+        end_date=request.json['end_date'],
+        is_finished=request.json['is_finished']
+    )
+    session.add(jobs)
+    session.commit()
+    return jsonify({'success': 'OK'})
