cfd3045e29508c84d209966912035a97f6c2f187
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-20 18:29:44 +0500

    Создана программа для тестирования users_resource.py

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 from requests import get, post, delete
 
-print(get('http://localhost:5000/api/v2/users').json())
-print(get('http://localhost:5000/api/v2/users/1').json())
+print(get('http://127.0.0.1:5000/api/v2/users').json())
+print(get('http://127.0.0.1:5000/api/v2/users/1').json())
+print(get('http://127.0.0.1:5000/api/v2/users/999').json())
+print(get('http://127.0.0.1:5000/api/v2/users/p').json())
 
-print(post('http://localhost:5000/api/v2/users').json())
-print(post('http://localhost:5000/api/v2/users',
-           json={'name': 'Маша', 'city_from': "Москва"}).json())
-print(post('http://localhost:5000/api/v2/users',
-           json={'job': 'Заголовок', 'team_leader': 1, "collaborators": '1, 2'}).json())
+print(delete('http://127.0.0.1:5000/api/v2/users/1').json())
+print(delete('http://127.0.0.1:5000/api/v2/users/999').json())
+print(delete('http://127.0.0.1:5000/api/v2/users/m').json())
 
-print(delete('http://localhost:5000/api/v2/users/1').json())
+print(post('http://127.0.0.1:5000/api/v2/users',
+           json={'surname': 'ла ла',
+                 'name': "ВАСЯ", 'age': 10,
+                 'city_from': "Тюмень", 'address': '', 'email': 'лала@mars.ru'}).json())
+print(post('http://127.0.0.1:5000/api/v2/users',
+           json={'surname': 'ла ла','age': 10,
+                 'city_from': "Тюмень"}).json())
+print(post('http://127.0.0.1:5000/api/v2/users',
+           json={'name': "Вася",
+                 'is_finished': False}).json())
