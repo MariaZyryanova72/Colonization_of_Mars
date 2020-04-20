e614a30f9e5a2a20f2e08fbadd421c32a13ac9da
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-20 18:28:29 +0500

    + Исправлены ошибки =) + Умеличен количество обязательных полей ввода в users.py

--- users_resource.py
+++ users_resource.py
@@ -4,15 +4,10 @@ from data import db_session
 from data.users import User
 
 parser = reqparse.RequestParser()
-parser.add_argument('surname', required=True)
-parser.add_argument('name', required=True)
-parser.add_argument('age', required=True, type=bool)
-parser.add_argument('city_from', required=True, type=int)
-parser.add_argument('position', required=True)
-parser.add_argument('speciality', required=True)
-parser.add_argument('address', required=True, type=bool)
-parser.add_argument('email', required=True, type=int)
-parser.add_argument('hashed_password', required=True, type=int)
+parser.add_argument('surname', required=True, type=str)
+parser.add_argument('name', required=True, type=str)
+parser.add_argument('city_from', required=True, type=str)
+parser.add_argument('email', required=True, type=str)
 
 
 def abort_if_user_not_found(user_id):
@@ -28,8 +23,7 @@ class UsersResource(Resource):
         session = db_session.create_session()
         user = session.query(User).get(user_id)
         return jsonify({'users': user.to_dict(
-            only=('surname', 'name', 'age', 'position', 'city_from',
-                  'speciality', 'address', 'email', 'hashed_password'))})
+            only=('surname', 'name', 'city_from', 'email'))})
 
     def delete(self, user_id):
         abort_if_user_not_found(user_id)
@@ -45,8 +39,7 @@ class UsersListResource(Resource):
         session = db_session.create_session()
         users = session.query(User).all()
         return jsonify({'users': [item.to_dict(
-            only=('surname', 'name', 'age', 'position', 'city_from',
-                  'speciality', 'address', 'email', 'hashed_password')) for item in users]})
+            only=('surname', 'name', 'city_from', 'email')) for item in users]})
 
     def post(self):
         args = parser.parse_args()
@@ -54,13 +47,8 @@ class UsersListResource(Resource):
         users = User(
             surname=args['surname'],
             name=args['name'],
-            age=args['age'],
-            position=args['position'],
             city_from=args['city_from'],
-            speciality=args['speciality'],
-            address=args['address'],
             email=args['email'],
-            hashed_password=args['hashed_password']
         )
         session.add(users)
         session.commit()
